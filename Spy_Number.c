937097e0fefce80eadaf6dba061ccf4b07371a78
yerukumkarthik <119839102+yerukumkarthik@users.noreply.github.com>
2022-12-06 11:24:37 +0530

    #include<stdio.h> int main() {     int n,b,i,sum=0,pro=1;     scanf("%d",&n);     for(i=n;i>0;i=i/10)     {         b=i%10;         sum+=b;         pro*=b;     }     if(sum==pro)     printf("Spy Number");     else     printf("Not Spy Number"); }

---
+++ Spy_Number.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n,b,i,sum=0,pro=1;
+    scanf("%d",&n);
+    for(i=n;i>0;i=i/10)
+    {
+        b=i%10;
+        sum+=b;
+        pro*=b;
+    }
+    if(sum==pro)
+    printf("Spy Number");
+    else
+    printf("Not Spy Number");
+}
