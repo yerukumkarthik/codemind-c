2fd5648970b4e7bdaefb87ba0fbec5404bdf17c7
yerukumkarthik <119839102+yerukumkarthik@users.noreply.github.com>
2022-12-27 10:27:13 +0530

    #include<stdio.h> int main() {     int n,i,j,z,c,co,a,s;     scanf("%d",&n);     for(i=1;i<n;i++)     {         c=0;         for(j=1;j<=i;j++)         {             if(i%j==0)             c++;         }         if(c==2)         {             for(z=1;z<n;z++)             {                 co=0;                 for(a=1;a<=n;a++)                 {                     if(z%a==0)                     co++;                 }                 if(co==2)                 {                     if(i*z==n)                     {                         printf("%d %d",i,z);                         s=0;                         break;                     }                 }                 if(s==0)                 break;             }         }         if(s==0)         break;     }     if(s!=0)     printf("-1"); }

---
+++ Find_two_distinct_prime_numbers_with_given_product.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j,z,c,co,a,s;
+    scanf("%d",&n);
+    for(i=1;i<n;i++)
+    {
+        c=0;
+        for(j=1;j<=i;j++)
+        {
+            if(i%j==0)
+            c++;
+        }
+        if(c==2)
+        {
+            for(z=1;z<n;z++)
+            {
+                co=0;
+                for(a=1;a<=n;a++)
+                {
+                    if(z%a==0)
+                    co++;
+                }
+                if(co==2)
+                {
+                    if(i*z==n)
+                    {
+                        printf("%d %d",i,z);
+                        s=0;
+                        break;
+                    }
+                }
+                if(s==0)
+                break;
+            }
+        }
+        if(s==0)
+        break;
+    }
+    if(s!=0)
+    printf("-1");
+}
