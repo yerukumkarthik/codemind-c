6945f7ee7fdd5f02fd9146212963961dd5f6bbbe
yerukumkarthik <119839102+yerukumkarthik@users.noreply.github.com>
2023-01-02 10:10:01 +0530

    #include<stdio.h> int main() {     int n,a,i;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         scanf("%d",&a);         if(a%2==0)         printf("%d ",a/2);         else         printf("%d ",a/2+1);     } }

---
+++ Playing_a_game_with_Hoops.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n,a,i;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        scanf("%d",&a);
+        if(a%2==0)
+        printf("%d
+",a/2);
+        else
+        printf("%d
+",a/2+1);
+    }
+}
