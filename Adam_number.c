4f0552d61da22e5e96983023bcc9438c085d6314
yerukumkarthik <119839102+yerukumkarthik@users.noreply.github.com>
2023-01-04 18:02:07 +0530

    #include<stdio.h> int main() {     int n,i,sq,b,s=0,k=0;     scanf("%d",&n);     sq=n*n;     for(i=n;i>0;i=i/10)     {         b=i%10;         s=s*10+b;     }     for(i=s*s;i>0;i=i/10)     {         b=i%10;         k=k*10+b;     }     if(sq==k)     printf("True");     else     printf("False"); }

--- Adam_number.c
+++ Adam_number.c
@@ -1,22 +1,20 @@
 #include<stdio.h>
 int main()
 {
-    int n,s=0,a,i,b,pro;
+    int n,i,sq,b,s=0,k=0;
     scanf("%d",&n);
-    a=n*n;
+    sq=n*n;
     for(i=n;i>0;i=i/10)
     {
         b=i%10;
         s=s*10+b;
     }
-    pro=s*s;
-    s=0;
-    for(i=pro;i>0;i=i/10)
+    for(i=s*s;i>0;i=i/10)
     {
         b=i%10;
-        s=s*10+b;
+        k=k*10+b;
     }
-    if(s==a)
+    if(sq==k)
     printf("True");
     else
     printf("False");
