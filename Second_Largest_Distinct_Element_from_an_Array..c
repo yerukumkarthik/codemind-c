50978ed876bf3a34b653e04caf0e9398d8127103
yerukumkarthik <119839102+yerukumkarthik@users.noreply.github.com>
2023-01-05 19:18:06 +0530

    #include<stdio.h> int main() {     int n,temp,i,j,arr[1000],a,k;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     scanf("%d",&a);     for(i=0;i<n;i++)     {         for(j=i+1;j<n;j++)         {             if(arr[i]<arr[j])             {                 temp=arr[i];                 arr[i]=arr[j];                 arr[j]=temp;             }         }     }     //printf("%d",arr[a-1]);     for(i=0;i<n;i++)     {         for(j=i+1;j<n;j++)         {             if(arr[i]==arr[j])             {                 for(k=j;k<n;k++)                 {                     arr[k]=arr[k+1];                 }                 j--;                 n--;             }         }     }     printf("%d ",arr[1]); }

---
+++ Second_Largest_Distinct_Element_from_an_Array..c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+int main()
+{
+    int n,temp,i,j,arr[1000],a,k;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    scanf("%d",&a);
+    for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(arr[i]<arr[j])
+            {
+                temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+            }
+        }
+    }
+    //printf("%d",arr[a-1]);
+    for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(arr[i]==arr[j])
+            {
+                for(k=j;k<n;k++)
+                {
+                    arr[k]=arr[k+1];
+                }
+                j--;
+                n--;
+            }
+        }
+    }
+    printf("%d ",arr[1]);
+}
