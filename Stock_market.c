58f6b9681d44435e51d1b5c350e7ac8fe937b258
yerukumkarthik <119839102+yerukumkarthik@users.noreply.github.com>
2023-01-06 23:11:22 +0530

    #include<stdio.h> int main() {     int n,i,a,b;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         scanf("%d%d",&a,&b);         if(a>b)         printf("LOSS ");         else if(a==b)         printf("NEUTRAL ");         else if(a<b)         printf("PROFIT ");     } }

---
+++ Stock_market.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,a,b;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        scanf("%d%d",&a,&b);
+        if(a>b)
+        printf("LOSS
+");
+        else if(a==b)
+        printf("NEUTRAL
+");
+        else if(a<b)
+        printf("PROFIT
+");
+    }
+}
