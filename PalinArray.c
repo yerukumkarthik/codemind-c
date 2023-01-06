97c8ffdf33418b5bb69fdac2261dee6f793503d3
yerukumkarthik <119839102+yerukumkarthik@users.noreply.github.com>
2023-01-06 23:09:02 +0530

    #include<stdio.h> int main() {     int arr[100],i,j,k=0,n,s,b,p;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         p=arr[i];         s=0;         while(arr[i]!=0)         {             b=arr[i]%10;             s=s*10+b;             arr[i]=arr[i]/10;         }         if(s==p)         {             k++;         }     }     if(k==n)     {         printf("1");     }     else     printf("0"); }

---
+++ PalinArray.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+    int arr[100],i,j,k=0,n,s,b,p;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        p=arr[i];
+        s=0;
+        while(arr[i]!=0)
+        {
+            b=arr[i]%10;
+            s=s*10+b;
+            arr[i]=arr[i]/10;
+        }
+        if(s==p)
+        {
+            k++;
+        }
+    }
+    if(k==n)
+    {
+        printf("1");
+    }
+    else
+    printf("0");
+}
