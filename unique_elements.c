4ec8d911b6a4fb26f4e953415d1a4220d68f37d7
yerukumkarthik <119839102+yerukumkarthik@users.noreply.github.com>
2023-01-19 19:08:46 +0530

    #include<stdio.h> int main() {     int n,i,j,c=1,s,arr[100],k;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         c=1;         for(j=i+1;j<n;j++)         {             if(arr[i]==arr[j])             {                c++;                for(k=j;k<n;k++)                {                    arr[k]=arr[k+1];                }                n--;                j--;             }         }     }     for(i=0;i<n;i++)     {         printf("%d ",arr[i]);     } }

---
+++ unique_elements.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j,c=1,s,arr[100],k;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        c=1;
+        for(j=i+1;j<n;j++)
+        {
+            if(arr[i]==arr[j])
+            {
+               c++;
+               for(k=j;k<n;k++)
+               {
+                   arr[k]=arr[k+1];
+               }
+               n--;
+               j--;
+            }
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        printf("%d ",arr[i]);
+    }
+}
